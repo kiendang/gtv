cf0a5dc4c61557cd6b0c50f0e872c0ce45754813
Kien Dang <kiend@proton.me>
2024-08-28 14:15:29 +0800

    Use express instead of node-http

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
 import { createServer } from "node:http";
 import "dotenv/config";
-import { grafserv } from "postgraphile/grafserv/node";
+import cors from "cors";
+import express from "express";
+import { grafserv } from "postgraphile/grafserv/express/v4";
 import { pgl } from "./pql";
 
 const serv = pgl.createServ(grafserv);
 
-const server = createServer();
+const app = express();
+app.use(cors());
+
+const server = createServer(app);
 server.on("error", (e) => {
   console.error(e);
 });
 
-serv.addTo(server).catch((e) => {
+serv.addTo(app, server).catch((e) => {
   console.error(e);
   process.exit(1);
 });
